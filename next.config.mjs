8d464a1cda337e5aab6d7758cd573a2445d37dd7
MARIESWARAN B <96181603+Marieswaran25@users.noreply.github.com>
2024-10-23 15:28:00 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output:'standalone',
     images: {
         unoptimized: true,
         domains: ['t3.ftcdn.net', 'cdn.shopify.com/', 'cdn.sanity.io', 'pngimg.com', 'img.thecdn.in'],
